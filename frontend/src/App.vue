f752f5556aa6ab994f9783cd5ec26c003c1e2c8c
wh0syx <wh0syx@proton.me>
2026-05-15 23:44:03 +0300

    Improve adaptivity of the entire application

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,17 @@
+<template>
+  <div class="repair-wrapper">
+    <Navbar />
+    <SearchForm />
+  </div>
+  <Companies />
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import SearchForm from './components/SearchForm.vue'
+import Companies from './components/Companies.vue'
+</script>
+
 <style>
 @font-face {
   font-family: "JetBrainsMono";
@@ -51,18 +65,45 @@ html, body, #root, .wrapper {
   background-color: #1538d4;
 }
 
+@media (max-width: 330px) {
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden !important;
+    background: #000 !important;
+  }
+
+  .repair-wrapper,
+  .repair-wrapper * {
+    visibility: hidden !important;
+  }
+
+  body::before {
+    content: "";
+    position: fixed;
+    inset: 0;
+    background: #000;
+    z-index: 2147483646;
+    pointer-events: none;
+  }
+
+  body::after {
+    content: ":(";
+    position: fixed;
+    inset: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    font-size: 72px;
+    line-height: 1;
+    z-index: 2147483647;
+    pointer-events: none;
+    transform: rotate(-270deg);
+    transform-origin: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+}
 </style>
-
-<template>
-  <div class="repair-wrapper">
-    <Navbar />
-    <SearchForm />
-  </div>
-  <Companies />
-</template>
-
-<script setup>
-import Navbar from './components/Navbar.vue'
-import SearchForm from './components/SearchForm.vue'
-import Companies from './components/Companies.vue'
-</script>
